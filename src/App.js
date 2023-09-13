4555f3f5ec0888100f0a746c7b5c9f1b789d57a5
su8ject <krivizyuk.andrei@gmail.com>
2023-09-13 20:35:13 +0300

    Підключив mui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import {Container, createTheme, IconButton, ThemeProvider} from "@mui/material";
+import {useMemo, useState} from "react";
+import {colors} from "./variables";
+import {Brightness2, Brightness7} from "@mui/icons-material";
 import "./style/reset.css";
+
 function App() {
-            <div>
-            </div>
+    const [mode, setMode] = useState("light");
+
+    const getDesignTokens = (mode) => ({
+        palette: {
+            mode,
+            ...(mode === 'light'
+                ? colors.lightMode
+                : colors.darkMode),
+        },
+    });
+
+    const theme = useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+
+    const switchTheme = () => {
+        (mode === "light") ? setMode("dark") : setMode("light");
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Container sx={{backgroundColor: "background.paper"}} maxWidth={false}>
+                <IconButton onClick={switchTheme}>
+                    {theme.palette.mode === "dark" ? <Brightness2/> : <Brightness7/>}
+                </IconButton>
+            </Container>
+        </ThemeProvider>
+    );
 }
 
 export default App;
---
+++ src/variables.js
@@ -0,0 +1,30 @@
+export const colors = {
+    lightMode: {
+        primary: {
+            light: "#474554",
+            main: "#929296",
+            dark: "#101010",
+            contrastText: "#bcbcbc",
+        },
+        secondary: {
+            light: "#fefefb",
+            main: "#b6bacb",
+            dark: "#8f8d8f",
+            contrastText: "#4d6560",
+        },
+    },
+    darkMode: {
+        primary: {
+            light: "#fefefb",
+            main: "#b6bacb",
+            dark: "#8f8d8f",
+            contrastText: "#4d6560",
+        },
+        secondary: {
+            light: "#474554",
+            main: "#929296",
+            dark: "#101010",
+            contrastText: "#bcbcbc",
+        },
+    },
+};
