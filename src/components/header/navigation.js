67a34ed6d04e46b1806ba2115a59fdd79719c0c0
su8ject <krivizyuk.andrei@gmail.com>
2023-09-19 11:51:03 +0300

    Update navigation.js

--- src/components/header/navigation.js
+++ src/components/header/navigation.js
@@ -13,7 +13,7 @@ export const Navigation = () => {
                 </StyledLink>
             </ListItem>
             <ListItem>
-                <StyledLink to="journal">
+                <StyledLink to="/journal">
                     <ImportContacts/>
                     Журнал
                 </StyledLink>
