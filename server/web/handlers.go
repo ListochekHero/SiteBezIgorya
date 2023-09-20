b25b0488b7ea4d8a85f29b4e94e6c17acf7c0e02
Serhii <artoryasabysswalker@gmail.com>
2023-09-20 18:09:03 +0300

    new helpers for logging

--- server/web/handlers.go
+++ server/web/handlers.go
@@ -10,13 +10,13 @@ import (
 func (app *application) mainWindowHandler(w http.ResponseWriter, r *http.Request) {
 	ts, err := template.ParseFiles("./ui/static/index.html")
 	if err != nil {
-		app.errorLog.Println(err.Error())
+		app.serverError(w, err)
 		http.Error(w, "Internal Server Error", 500)
 		return
 	}
 	err = ts.Execute(w, nil)
 	if err != nil {
-		app.errorLog.Println(err.Error())
+		app.serverError(w, err)
 		http.Error(w, "Internal Server Error", 500)
 	}
 }
@@ -25,17 +25,17 @@ func (app *application) showSomeJournal(w http.ResponseWriter, r *http.Request) 
 
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.NotFound(w)
 		return
 	}
-	fmt.Fprintf(w, "Отображение выбранной заметки с ID %d...", id)
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func (app *application) createSomeJournal(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Метод запрещен!", http.StatusMethodNotAllowed)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
-	w.Write([]byte("Отображение заметки..."))
+	w.Write([]byte("Создание новой заметки..."))
 }
