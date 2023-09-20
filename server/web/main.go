b36aa83c1ffc08066b959e4780186d2f81445cec
Serhii <artoryasabysswalker@gmail.com>
2023-09-20 19:29:27 +0300

    some refactoring for routes
    added new file for refactoring -> routes.go and now all actual routes should be eddited there. Also transfered part of file system code to the same file

--- server/web/main.go
+++ server/web/main.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 )
 
 type application struct {
@@ -35,47 +34,14 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.mainWindowHandler)
-	mux.HandleFunc("/journal", app.mainWindowHandler)
-	mux.HandleFunc("/login", app.mainWindowHandler)
-	mux.HandleFunc("/newPost", app.mainWindowHandler)
-
-	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static/")})
-	mux.Handle("/static", http.NotFoundHandler())
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	infoLog.Printf("Server is running on :%s\n", *addr)
 
 	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
-}
-
-type neuteredFileSystem struct {
-	fs http.FileSystem
-}
-
-func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
-	f, err := nfs.fs.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	s, err := f.Stat()
-	if s.IsDir() {
-		index := filepath.Join(path, "index2.html")
-		if _, err := nfs.fs.Open(index); err != nil {
-			closeErr := f.Close()
-			if closeErr != nil {
-				return nil, closeErr
-			}
-			return nil, err
-		}
-	}
-	return f, nil
 }
---
+++ server/web/routes.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+	"path/filepath"
+)
+
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", app.mainWindowHandler)
+	mux.HandleFunc("/journal", app.mainWindowHandler)
+	mux.HandleFunc("/login", app.mainWindowHandler)
+	mux.HandleFunc("/newPost", app.mainWindowHandler)
+
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static/")})
+	mux.Handle("/static", http.NotFoundHandler())
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	return mux
+}
+
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	s, err := f.Stat()
+	if s.IsDir() {
+		index := filepath.Join(path, "index2.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+			return nil, err
+		}
+	}
+	return f, nil
+}
