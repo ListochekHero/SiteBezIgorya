e61dd77356b64830032f56fe2e07789f587e69c9
su8ject <krivizyuk.andrei@gmail.com>
2023-09-22 14:33:05 +0300

    Create static.js

---
+++ src/static.js
@@ -0,0 +1,11 @@
+export const loginInputs = [
+    {
+        label: "Введіть логін",
+        description: "Логін:",
+        field: "login",
+    },
+    {
+        label: "Введіть пароль",
+        description: "Пароль:",
+        field: "password",
+    }];
