379d3cad8ddf981dac3e01c83165610457b2ad0b
su8ject <krivizyuk.andrei@gmail.com>
2023-09-22 14:36:51 +0300

    Розмітка головної сторінки

--- src/views/main/index.js
+++ src/views/main/index.js
@@ -1,12 +1,31 @@
-import {Typography} from "@mui/material";
+import {Box, Container, styled, Typography} from "@mui/material";
 import {MainContainer} from "../../components/UI/mainContainer";
+import {InfoCard} from "./infoCard";
+import {API} from "../../api";
 
 export const Main = () => {
+
+    const cVs = API.getCV();
+
     return (
         <MainContainer>
-            <Typography>
-                MAIN
-            </Typography>
+            <Box>
+                <Typography variant="h1" component="h2" align="center">INFO</Typography>
+            </Box>
+            <WrapperCards maxWidth="false">
+                {cVs.map((card) => (
+                    <InfoCard
+                        props={card}
+                        key={card.id}
+                    />
+                ))}
+            </WrapperCards>
         </MainContainer>
     );
 };
+
+const WrapperCards = styled(Container)(() => ({
+    display: "flex",
+    justifyContent: "space-around",
+    flexWrap: "wrap",
+}));
