3c354cc65f0eccef43f2e8e17bfcd0020eae4809
su8ject <krivizyuk.andrei@gmail.com>
2023-09-30 15:29:40 +0300

    Додав роути до menuNavigation та стилізував покликання

--- src/components/header/navigationMenu/menuList.js
+++ src/components/header/navigationMenu/menuList.js
@@ -1,4 +1,5 @@
 import {Menu, MenuItem, styled} from "@mui/material";
+import {NavLink} from "react-router-dom";
 
 export const MenuList = ({handleMenu, isActive}) => {
     const anchorEl = document.querySelector("#header");
@@ -17,9 +18,21 @@ export const MenuList = ({handleMenu, isActive}) => {
                 horizontal: 'right',
             }}
         >
-            <StyledItem>Team</StyledItem>
-            <StyledItem>Sprints</StyledItem>
-            <StyledItem>Log in</StyledItem>
+            <StyledItem>
+                <StyledLink to={"/"} onClick={handleMenu}>
+                    Team
+                </StyledLink>
+            </StyledItem>
+            <StyledItem>
+                <StyledLink to={"/journal"} onClick={handleMenu}>
+                    Sprints
+                </StyledLink>
+            </StyledItem>
+            <StyledItem>
+                <StyledLink to={"/login"} onClick={handleMenu}>
+                    Log in
+                </StyledLink>
+            </StyledItem>
         </StyledMenu>
     );
 };
@@ -38,7 +51,21 @@ const StyledMenu = styled(Menu)(({theme}) => ({
 }));
 
 const StyledItem = styled(MenuItem)(({theme}) => ({
-    padding: "5px 20px",
+    height: "40px",
     backgroundColor: theme.palette.primary.main,
+    padding: "0",
     ":hover": {},
+}));
+
+const StyledLink = styled(NavLink)(({theme}) => ({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: "100%",
+    textDecoration: "none",
+    color: theme.palette.text.primary,
+    "&.active": {
+        backgroundColor: theme.palette.primary.light,
+    },
 }));
