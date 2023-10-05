76cd270b62e9c8ef693a38bc355407b1a72be632
su8ject <krivizyuk.andrei@gmail.com>
2023-10-05 17:20:36 +0300

    Переіменував роут та компонент, пов'язаний з ним

--- src/components/appRouter.js
+++ src/components/appRouter.js
@@ -1,7 +1,7 @@
 import {Route, Routes} from "react-router-dom";
 import {Main} from "../views/main";
 import {Journal} from "../views/journal";
-import {NewPost} from "../views/newPost";
+import {NewSprint} from "../views/newSprint";
 import {Login} from "../views/login";
 
 export const AppRouter = () => {
@@ -10,7 +10,7 @@ export const AppRouter = () => {
             <Route index element={<Main/>}/>
             <Route path="journal" element={<Journal/>}/>
             <Route path="login" element={<Login/>}/>
-            <Route path="newPost" element={<NewPost/>}/>
+            <Route path="newSprint" element={<NewSprint/>}/>
             <Route path="*" element={<Main/>}/>
         </Routes>
     );
