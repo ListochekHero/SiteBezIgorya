3fc0feae0a0eb08949c93b1f70869c2915314b8e
Serhii <artoryasabysswalker@gmail.com>
2023-10-07 04:33:19 +0300

    changes related to MVP release
    refactoring of mongo connection, seve file function and preparations before MVP release

--- server/web/helpers.go
+++ server/web/helpers.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 	"runtime/debug"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
 	app.errorLog.Output(2, trace)
-
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
 
@@ -19,4 +25,32 @@ func (app *application) clientError(w http.ResponseWriter, status int) {
 
 func (app *application) NotFound(w http.ResponseWriter) {
 	app.clientError(w, http.StatusNotFound)
+}
+
+func saveFile(file io.Reader, whatToSave string, identificator any) error {
+	fileToCreate := fmt.Sprintf("%v%v.png", whatToSave, identificator)
+	filePath := filepath.Join("static", fileToCreate)
+	newFile, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer newFile.Close()
+	_, err = io.Copy(newFile, file)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func connectToMongoDB() (*mongo.Client, error) {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://goWebUser:gowebuser@localhost:27017/goWebDB"))
+	if err != nil {
+		return nil, err
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("Connected to MongoDB!")
+	return client, nil
 }
