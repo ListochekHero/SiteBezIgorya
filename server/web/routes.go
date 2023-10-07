11afe23570d778c3a0a9e7d81ac5bf456b9eca39
Serhii <artoryasabysswalker@gmail.com>
2023-10-07 04:31:12 +0300

    MVP version of routes
    delete unwanted routes and add needed rutes to project

--- server/web/routes.go
+++ server/web/routes.go
@@ -8,17 +8,13 @@ import (
 func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.mainWindowHandler)
-	mux.HandleFunc("/journal", app.showSomeJournal)
-	mux.HandleFunc("/login", app.mainWindowHandler)
-	mux.HandleFunc("/newPost", app.createSomeJournal)
 	mux.HandleFunc("/team/CVs/", app.sendCVs)
-	mux.HandleFunc("/saveSpint/", app.saveSpint)
+	mux.HandleFunc("/saveSprint/", app.saveSpint)
 	mux.HandleFunc("/sprintsCount/", app.sprintsCount)
-
-	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static/")})
+	mux.HandleFunc("/getSprint", app.sendSprint)
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("static/")})
 	mux.Handle("/static", http.NotFoundHandler())
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
 	return mux
 }
 
@@ -32,6 +28,9 @@ func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
 		return nil, err
 	}
 	s, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
 	if s.IsDir() {
 		index := filepath.Join(path, "index2.html")
 		if _, err := nfs.fs.Open(index); err != nil {
