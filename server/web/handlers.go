91e61bbe96a5e33a2401b03927b78a5f0dd11915
Serhii <artoryasabysswalker@gmail.com>
2023-10-07 04:39:18 +0300

    MVP realese preparations
    added all needed routes handlers for front and DB communication

--- server/web/handlers.go
+++ server/web/handlers.go
@@ -4,16 +4,15 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
 	"text/template"
 
-	"context"
-
 	"botgap.duo.com/SiteBezIgorya/pkg/models"
-	"go.mongodb.org/mongo-driver/bson"
 )
 
 func (app *application) mainWindowHandler(w http.ResponseWriter, r *http.Request) {
-	ts, err := template.ParseFiles("./ui/static/index.html")
+	ts, err := template.ParseFiles("./static/index.html")
 	if err != nil {
 		app.serverError(w, err)
 		http.Error(w, "Internal Server Error", 500)
@@ -26,97 +25,133 @@ func (app *application) mainWindowHandler(w http.ResponseWriter, r *http.Request
 	}
 }
 
-func (app *application) showSomeJournal(w http.ResponseWriter, r *http.Request) {
-	collection := app.mongoDBModel.Client.Database("goWebDB").Collection("CVs")
-	result := collection.FindOne(context.TODO(), bson.M{"someid": 1})
-	var cv models.CV
-	if err := result.Decode(&cv); err != nil {
-		app.errorLog.Fatal(err)
-	}
-	// id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	// if err != nil || id < 1 {
-	// 	app.NotFound(w)
-	// 	return
-	// }
-	fmt.Fprintf(w, "Display a specific snippet with ID %v...", cv)
-}
-
-func (app *application) createSomeJournal(w http.ResponseWriter, r *http.Request) {
-
-	collection := app.mongoDBModel.Client.Database("goWebDB").Collection("Comments")
-	comModel := models.CommentforDB{
-		IDofSprint:  1,
-		Name:        "Serhii",
-		Description: "can run some Go app",
-	}
-	// cvModel := models.CV{
-	// 	IDofCV:      1,
-	// 	Name:        "Andrii Kryvyziuk",
-	// 	DevStatus:   "front-end developer",
-	// 	Description: "Sample description",
-	// 	URLAvatar:   "https://prod.api.assets.riotgames.com/public/v1/asset/lol/13.18.1/CHAMPION/497/ICON?width=200&height=200&format=auto&auto=webp",
-	// 	GitHub:      "https://github.com/su8ject",
-	// 	CV:          "CV data",
-	// 	Portfolio:   "https://su8ject.github.io/portfolio/",
-	// 	Telegram:    "https://t.me/AndriiKryvyziuk",
-	// }
-	result, err := collection.InsertOne(context.TODO(), comModel)
-	if err != nil {
-		app.errorLog.Fatal(err)
-	}
-	insertedID := result.InsertedID
-	fmt.Fprintf(w, "Inserted document ID:%v", insertedID)
-}
-
 func (app *application) sendCVs(w http.ResponseWriter, r *http.Request) {
-	//id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	// if err != nil || id < 1 {
-	// 	app.NotFound(w)
-	// 	return
-	// }
-
 	cvs, err := app.mongoDBModel.GetAllCVs()
 	if err != nil {
-		http.Error(w, "Unable to retrieve CV data from database", http.StatusInternalServerError)
+		http.Error(w, "Unable to retrieve CVs data from database", http.StatusInternalServerError)
 		app.errorLog.Fatal(err)
-		return
 	}
-
 	cvJSON, err := json.Marshal(cvs)
 	if err != nil {
-		http.Error(w, "Unable to encode CV data to JSON", http.StatusInternalServerError)
+		http.Error(w, "Unable to encode CVs data to JSON", http.StatusInternalServerError)
 		app.errorLog.Fatal(err)
-		return
 	}
-
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(cvJSON)
 }
 
 func (app *application) saveSpint(w http.ResponseWriter, r *http.Request) {
-	// Читаємо JSON з тіла HTTP-запиту
-	var sprint models.SprintfromFront
-	err := json.NewDecoder(r.Body).Decode(&sprint)
+	err := r.ParseMultipartForm(10 << 20)
 	if err != nil {
-		http.Error(w, "Помилка розкодування JSON", http.StatusBadRequest)
-		return
+		http.Error(w, "Unable to parse request", http.StatusInternalServerError)
+		app.errorLog.Fatal(err)
 	}
-
-	// Опрацьовуємо дані sprint
-
-	// Надсилаємо відповідь назад на фронтенд
+	file, _, err := r.FormFile("snapshot")
+	if err != nil {
+		http.Error(w, "Unable to get file", http.StatusInternalServerError)
+		app.errorLog.Fatal(err)
+	}
+	defer file.Close()
+	newSprintID, err := app.mongoDBModel.DocumentsCount(app.mongoDBModel.Client, "goWebDB", "Sprints")
+	if err != nil {
+		app.errorLog.Fatal(err)
+	}
+	newSprintID++
+	err = saveFile(file, "sprint", newSprintID)
+	if err != nil {
+		http.Error(w, "Unable to save file", http.StatusInternalServerError)
+		app.errorLog.Fatal(err)
+	}
+	stateJSON := r.FormValue("json")
+	var sprint models.SprintFromFront
+	err = json.Unmarshal([]byte(stateJSON), &sprint)
+	if err != nil {
+		http.Error(w, "Unable to decode JSON", http.StatusBadRequest)
+		app.errorLog.Fatal(err)
+	}
+	sprintToInsert := models.SprintForDB{
+		IDofSprint:   newSprintID,
+		Title:        sprint.Title,
+		Date:         "some date",
+		SnapshotURL:  "http://134.249.147.135/static/sprint" + strconv.Itoa(newSprintID) + ".png",
+		Descriptions: sprint.Descriptions,
+	}
+	err = app.mongoDBModel.InsertSprint(sprintToInsert)
+	if err != nil {
+		app.errorLog.Fatal(err)
+	}
+	var commentsDB []interface{}
+	for _, comment := range sprint.Comments {
+		commentDB := models.CommentForDB{
+			IDofSprint: newSprintID,
+			Name:       comment.Name,
+			Comment:    comment.Comment,
+		}
+		commentsDB = append(commentsDB, commentDB)
+	}
+	err = app.mongoDBModel.InsertComments(commentsDB)
+	if err != nil {
+		app.errorLog.Fatal(err)
+	}
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Дані успішно отримано та опрацьовано"))
+	w.Write([]byte("Everything is OK"))
 }
 
 func (app *application) sprintsCount(w http.ResponseWriter, r *http.Request) {
-	count, err := app.mongoDBModel.DocumentsCount(app.mongoDBModel.Client, "gCoWebDB", "Sprints")
+	count, err := app.mongoDBModel.DocumentsCount(app.mongoDBModel.Client, "goWebDB", "Sprints")
 	if err != nil {
-		http.Error(w, "Unable to retrieve doucments count data from database", http.StatusInternalServerError)
+		http.Error(w, "Unable to retrieve documents count data from database", http.StatusInternalServerError)
 		app.errorLog.Fatal(err)
-		return
 	}
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "%d", count)
+}
+
+func (app *application) sendSprint(w http.ResponseWriter, r *http.Request) {
+	idofsprint, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || idofsprint < 1 {
+		app.NotFound(w)
+		return
+	}
+	sprint, err := app.mongoDBModel.GetSprintByID(idofsprint)
+	if err != nil {
+		http.Error(w, "Unable to retrieve CVs data from database", http.StatusInternalServerError)
+		app.errorLog.Fatal(err)
+	}
+	comments, err := app.mongoDBModel.GetCommentsByID(idofsprint)
+	if err != nil {
+		http.Error(w, "Unable to retrieve Comments data from database", http.StatusInternalServerError)
+		app.errorLog.Fatal(err)
+	}
+	cvs, err := app.mongoDBModel.GetAllCVs()
+	if err != nil {
+		http.Error(w, "Unable to retrieve CVs data from database", http.StatusInternalServerError)
+		app.errorLog.Fatal(err)
+	}
+	for i := range comments {
+		comment := &comments[i]
+		for _, cv := range cvs {
+			if strings.Contains(cv.Name, comment.Name) {
+				comment.DevStatus = cv.DevStatus
+				comment.URLAvatar = cv.URLAvatar
+				break
+			}
+		}
+	}
+	sprintToSend := models.SprintForFront{
+		Title:        sprint.Title,
+		Date:         sprint.Date,
+		SnapshotURL:  sprint.SnapshotURL,
+		Descriptions: sprint.Descriptions,
+		Comments:     comments,
+	}
+	sprintToSendJSON, err := json.Marshal(sprintToSend)
+	if err != nil {
+		http.Error(w, "Unable to encode requested data to JSON", http.StatusInternalServerError)
+		app.errorLog.Fatal(err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(sprintToSendJSON)
 }
